430e6596706f4e249c9c10141954d36215978cc1
ninjapiraatti <tuoppil@gmail.com>
2021-11-25 18:20:18 -0100

    Add test for random aninmal

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aninmals
+
+This little crate has lots of cool aninmals like aquatic sock puppets, fart squirrels etc.
--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,71 @@
-#[derive(Debug)]
-struct Aninmal {
+use rand::{thread_rng, Rng};
+
+#[derive(Debug, Clone)]
+pub struct Aninmal {
     name: &'static str,
-    age: i32,
+    sound: &'static str,
+    toesywoesies: i32,
 }
 
 const ANINMALS: [Aninmal; 21] = [
-    Aninmal { name: "Disco turkey", age: 1 },
-    Aninmal { name: "Birb", age: 1 },
-    Aninmal { name: "Formal chikcen", age: 1 },
-    Aninmal { name: "Treefloof", age: 1 },
-    Aninmal { name: "Nope rope", age: 1 },
-    Aninmal { name: "Danger noodle", age: 1 },
-    Aninmal { name: "Sea flap flap", age: 1 },
-    Aninmal { name: "Murder log", age: 1 },
-    Aninmal { name: "Wizard cow", age: 1 },
-    Aninmal { name: "Flopwop", age: 1 },
-    Aninmal { name: "Danger zebra", age: 1 },
-    Aninmal { name: "Stab rabbit", age: 1 },
-    Aninmal { name: "Fart squirrel", age: 1 },
-    Aninmal { name: "Blub blub doggo", age: 1 },
-    Aninmal { name: "Trouble bubble", age: 1 },
-    Aninmal { name: "Aquatic sock puppet", age: 1 },
-    Aninmal { name: "Water pistachio", age: 1 },
-    Aninmal { name: "Cheese boi", age: 1 },
-    Aninmal { name: "Noodle bear", age: 1 },
-    Aninmal { name: "Booblesnoot", age: 1 },
-    Aninmal { name: "Murder torpedo", age: 1 },
+    Aninmal { name: "Disco turkey", sound: "Cluck!", toesywoesies: 8 },
+    Aninmal { name: "Birb", sound: "Tweet!", toesywoesies: 8 },
+    Aninmal { name: "Formal chikcen", sound: "Päkäkäkää!", toesywoesies: 8 },
+    Aninmal { name: "Treefloof", sound: "Blerb!", toesywoesies: 20 },
+    Aninmal { name: "Nope rope", sound: "Hisssss...", toesywoesies: 0 },
+    Aninmal { name: "Danger noodle", sound: "Hisssss...", toesywoesies: 0 },
+    Aninmal { name: "Sea flap flap", sound: "Blub", toesywoesies: 0 },
+    Aninmal { name: "Murder log", sound: "Snap!", toesywoesies: 20 },
+    Aninmal { name: "Wizard cow", sound: "Bah!", toesywoesies: 8 },
+    Aninmal { name: "Flopwop", sound: "Squeek!", toesywoesies: 20 },
+    Aninmal { name: "Danger zebra", sound: "Roar!", toesywoesies: 20 },
+    Aninmal { name: "Stab rabbit", sound: "Squaw!", toesywoesies: 20 },
+    Aninmal { name: "Fart squirrel", sound: "Skrit skrit!", toesywoesies: 20 },
+    Aninmal { name: "Blub blub doggo", sound: "Blub blub", toesywoesies: 0 },
+    Aninmal { name: "Trouble bubble", sound: "...", toesywoesies: 0 },
+    Aninmal { name: "Aquatic sock puppet", sound: "Blooorb!", toesywoesies: 0 },
+    Aninmal { name: "Water pistachio", sound: "Clack!", toesywoesies: 0 },
+    Aninmal { name: "Cheese boi", sound: "Qwik!", toesywoesies: 20 },
+    Aninmal { name: "Noodle bear", sound: "Krit krit.", toesywoesies: 20 },
+    Aninmal { name: "Booblesnoot", sound: "Om nom.", toesywoesies: 20 },
+    Aninmal { name: "Murder torpedo", sound: "Redrum!", toesywoesies: 0 },
 ];
 
-pub fn get_random_aninmal_name() {
-    let mut all_aninmals: Vec<Aninmal> = Vec::new();
-    println!("{:?}", ANINMALS);
+pub trait Noisemaker {
+    fn make_noise(&self);
+}
+
+impl Noisemaker for Aninmal {
+    fn make_noise(&self) {
+        println!("{:?}", self.sound);
+    }
+}
+pub fn get_all_aninmals() -> Vec<Aninmal> {
+    get_random_aninmal().make_noise();
+    ANINMALS.to_vec()
+}
+
+pub fn get_random_aninmal() -> Aninmal {
+    let mut rng = thread_rng();
+    let x: usize = rng.gen_range(0, ANINMALS.len());
+    ANINMALS[x].clone()
 }
 
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn has_aninmals() {
-        use crate::get_random_aninmal_name;
+        use crate::get_all_aninmals;
         use crate::ANINMALS;
-        //println!("{:?}", ANINMALS);
+        get_all_aninmals();
         assert_eq!(ANINMALS[0].name, "Disco turkey");
+    }
+    #[test]
+    fn can_has_random_aninmal() {
+        use crate::get_random_aninmal;
+        let random_aninmal = get_random_aninmal();
+        assert_ne!(random_aninmal.name.len(), 0);
+        assert_ne!(random_aninmal.sound.len(), 0);
     }
 }
